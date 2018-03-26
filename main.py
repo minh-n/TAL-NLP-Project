5a2b6fa0150d45eeeb2ce7cc05ca31888d210291
shipanda01 <hien.minh1996@gmail.com>
2018-03-26 14:32:36 +0200

    mode 1

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import random 
 
 
+def mode1():
+	x = input("User:")
+	answer = random.randInt(0,)
 
-x = input("User:")
+	print()
 
