d5271760bfebc2baac84acc2230aa2789b614b55
shipanda01 <hien.minh1996@gmail.com>
2018-03-26 14:52:01 +0200

    premier commit

--- main.py
+++ main.py
@@ -4,10 +4,30 @@
 import numpy as np
 import random 
 
+listModeOne = ["hmm", "ehh", "ahh", "ok", "lol", "yes", "right", "good", "huhu", "haha"]
 
-def mode1():
-	x = input("User:")
-	answer = random.randInt(0,)
+answerModeOne = ""
 
-	print()
+def modeOne():
 
+	global answerModeOne
+	
+	x = input("User: ")
+
+	loop = True
+
+	while(loop):
+		answerNum = random.randint(0,len(listModeOne)-1)
+		answer = listModeOne[answerNum]
+		if(answer != answerModeOne):
+			loop = False
+
+	print("Bot: " + answer)
+ 
+	answerModeOne = answer
+
+
+if __name__=="__main__":	
+	
+	while(True):
+		modeOne()
