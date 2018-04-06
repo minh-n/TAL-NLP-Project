1a423c596c54897ace69bbf1ce460555bee338fb
shipanda01 <hien.minh1996@gmail.com>
2018-04-06 10:15:58 +0200

    Fusion avec mode2 Veados

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import re
+import os
 import random 
 import nltk 
 from nltk.tokenize import word_tokenize
@@ -44,15 +46,61 @@ def printAnswer(answer):
 	print("\tBot: " + answer)
 
 
-def modeTwo():
-	return
+def modeTwo(sent, dicoModeTwo, answerModeTwo, answerAI, answerCharacter, answerEmotion, answerEnvironment, answerInfo):
+    tag = None
+    answer = ""
+    
+    #for cle, value in dicoModeTwo.items():
+        #print("mot : {}, tag : {}".format(cle, value))
+    for word in sent:
+        #print(word)
+        if word in dicoModeTwo:
+            tag = dicoModeTwo[word]
+            break
 
+    if tag != None:
+        if tag == "tagAI.txt":
+            answer = modeOne(answerAI, answerModeTwo)
+        elif tag == "tagCharacter.txt":
+            answer = modeOne(answerCharacter, answerModeTwo)
+        elif tag == "tagEmotion.txt":
+            answer = modeOne(answerEmotion, answerModeTwo)
+        elif tag == "tagEnvironment.txt":
+            answer = modeOne(answerEnvironment, answerModeTwo)
+        else:
+            answer = modeOne(answerInfo, answerModeTwo)
+        
+    return answer
 
 def contains(word):
 	return
 
 def createDict():
+    dico = {}
+    for element in os.listdir("../data/dataModeTwo"):
+        with open("../data/dataModeTwo/" + element, "r") as fp:
+            for line in fp.readlines():
+                if line.strip() == "": continue 	#skipping empty lines
+                dico[line] = element	
+        
+    return dico
+
+def tokenise_en(sent):
+    sent = re.sub("([^ ])\'", r"\1 '", sent) # separate apostrophe from preceding word by a space if no space to left
+    sent = re.sub(" \'", r" ' ", sent) # separate apostrophe from following word if a space if left
+
+    # separate on punctuation
+    cannot_precede = ["M", "Prof", "Sgt", "Lt", "Ltd", "co", "etc", "[A-Z]", "[Ii].e", "[eE].g"] # non-exhaustive list
+    regex_cannot_precede = "(?:(?<!"+")(?<!".join(cannot_precede)+"))"
+    sent = re.sub(regex_cannot_precede+"([\.\,\;\:\)\(\"\?\!]( |$))", r" \1", sent)
+    sent = re.sub("((^| )[\.\?\!]) ([\.\?\!]( |$))", r"\1\2", sent) # then restick several fullstops ... or several ?? or !!
+    sent = sent.split() # split on whitespace
+    return sent
+
+
+def tokenizeQuestion():
 	return
+
 
 
 #tokenize a given sentence, using nltk's word_tokenize 
@@ -83,18 +131,30 @@ def modeThree():
 
 
 
-if __name__=="__main__":	
-  
-	listModeOne = []
-	listModeOne = readDataLine("../data/dataModeOne.txt")
-	answerModeOne = ""
-	
-	print("\nWelcome to the chatbot simulation.\n")
+if __name__=="__main__":
+    listModeOne = []
+    listModeOne = readDataLine("../data/dataModeOne.txt")
+    answerModeOne = ""
+    answerModeTwo = ""
+    dicoModeTwo = createDict()
+    user = ""
+    
+    answerAI = readDataLine("../data/dataModeTwoAnswers/answerAI.txt")
+    answerCharacter = readDataLine("../data/dataModeTwoAnswers/answerCharacter.txt")
+    answerEmotion = readDataLine("../data/dataModeTwoAnswers/answerEmotion.txt")
+    answerEnvironment = readDataLine("../data/dataModeTwoAnswers/answerEnvironment.txt")
+    answerInfo = readDataLine("../data/dataModeTwoAnswers/answerInfo.txt")
 
-	while(True):
-
-
-		inputUser = raw_input("User: ")
-		tokenizeSentence(inputUser)
-
-		answerModeOne = modeOne(listModeOne, answerModeOne)
+    while(user != "quit"):
+        user = raw_input("User: ")
+        if user == "quit":
+            printAnswer("Bye")
+        else:
+            sent = tokenise_en(user)
+            answerModeTwo = modeTwo(sent, dicoModeTwo, answerModeTwo, answerAI, answerCharacter, answerEmotion, answerEnvironment, answerInfo)
+            if answerModeTwo != "":
+                printAnswer(answerModeTwo)
+            else:
+                answerModeOne = modeOne(listModeOne, answerModeOne)
+                printAnswer(answerModeOne)
+        
