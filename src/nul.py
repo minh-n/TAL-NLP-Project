5108449d37d09ee26001dd3daabe9d516100928e
neofoetus <corentin.manscour@u-psud.fr>
2018-05-05 20:37:44 +0200

    fonc aux 1

---
+++ src/nul.py
@@ -0,0 +1,20 @@
+
+
+def nbToWord(tag, state):
+	string = ""
+	if tag == "sleep_state":
+		string += "sleeping"
+	else :
+		if tag == "food_state":
+			string += "eating"
+		else :
+			string += "hydrated"
+
+	if state > 7 :
+		string += " well"
+	else :
+		if state > 3 & <= 7 :
+			string += "normally"
+
+		else :
+			string = "not "+" at all"
